332f57d54ddf33ebdc745befd9e17acf8a912304
yzy-1 <50034950+yzy-1@users.noreply.github.com>
2022-07-29 19:38:09 +0800

    chore(*): change default postgres user to "root"

--- start-postgres.sh
+++ start-postgres.sh
@@ -3,8 +3,8 @@
 docker run -d \
 	--name rindag-postgres \
   -e POSTGRES_USER=root \
-  -e POSTGRES_DB=rindag \
 	-e POSTGRES_PASSWORD=root \
+  -e POSTGRES_DB=rindag \
 	-e PGDATA=/var/lib/postgresql/data/pgdata \
 	-v /var/lib/postgresql/data:/var/lib/postgresql/data \
   -p 5432:5432 \
